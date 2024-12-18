279256aab40f66078a6fdeb6963747ad0078ef4c
acilegna <acilegna.airam88@gmail.com>
2024-12-18 15:37:19 -0600

    prueba

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,27 +1,27 @@
 class TasksController < ApplicationController
-  
-   
-   def import
-    file = params[:file]
-    return redirect_to users_path, notice: 'Only CSV please' unless file.content_type == 'text/csv'
-
-    CsvImportUsersService.new.call(file)
-
-    redirect_to users_path, notice: 'Users imported!'
+  def shows1
+    @task=  Task.order("likes DESC").limit(2)
+    render json: @task
   end
 
-  
+  def shows2
+     @task= Task.count
+    
+   #render json: @task
+  end
 
-  def allTasks
-      @task=  Task.order("likes DESC").limit(2)
-   
-    respond_to do |format|
-      format.html
-      format.csv do
-        filename = ['Task', Date.today.to_s].join(' ')
-        send_data User.to_csv(@task), filename:, content_type: 'text/csv'
-      end
-    end
+   def allTasks
+      @task= Task.where(status: ["Pendiente","Terminado"])
+
+      
+  respond_to do |format|
+    format.xlsx {
+      response.headers[
+        'Content-Disposition'
+      ] = "attachment; filename='all.xlsx'"
+    }
+    format.html { render :allTasks }
+  end
   end
 
   
