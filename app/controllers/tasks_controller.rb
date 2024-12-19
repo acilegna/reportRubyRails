2679f37b6321b45ce9efe67ca18e6e0028ea86f6
acilegna <acilegna.airam88@gmail.com>
2024-12-19 00:27:48 -0600

    exportar excel finish

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,24 +1,33 @@
-class TasksController < ApplicationController
-  
-   
-   
+class TasksController < ApplicationController 
+
+ 
 
  
 
   
-def index
+def popular
    @tasks=  Task.order("likes DESC").limit(2)
-   
+    @total= Task.count
+
     respond_to do |format|
       format.html
       format.xlsx {
-        response.headers['Content-Disposition'] = 'attachment; filename="all_task.xlsx"'
+        response.headers['Content-Disposition'] = 'attachment; filename="popular.xlsx"'
       }
     end
   end
 
-  
-  
+    def pendingFinish
+ 
+   @tasks= Task.where(status: ["Pendiente","Terminado"])
+
+    respond_to do |format|
+      format.html
+      format.xlsx {
+        response.headers['Content-Disposition'] = 'attachment; filename="tasks.xlsx"'
+      }
+    end
+  end
 
 
   
