daad462210da6f6dbaab154dba3dd9c66b2a72db
acilegna <acilegna.airam88@gmail.com>
2024-12-23 17:05:48 -0600

    api exportar excel headers

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,18 +1,42 @@
  
 class ApiController < ApplicationController
 
-  def index
+
+  def reportes(table)
     require 'csv'
     $user=ENV['USERNAME']
-    file = "C:/Users/#{$user}/Downloads/data.csv"
-    table = Task.where(status: ["Pendiente","Terminado"]);
-    
+    file = "C:/Users/#{$user}/Downloads/data.csv"        
     CSV.open( file, 'w' ) do |writer|
       writer << table.first.attributes.map { |a,v| a }
       table.each do |s|
         writer << s.attributes.map { |a,v| v }
       end
     end
-  end
+  end  
   
+  def totalTask
+
+    require 'csv' 
+    $user=ENV['USERNAME']
+    file = "C:/Users/#{$user}/Downloads/data.csv"    
+    products = Task.order("likes DESC").limit(2)
+    headers = ["Product ID", "Name", "Price", "Description"]
+CSV.open(file, 'w', write_headers: true, headers: headers) do |writer|
+  products.each do |product| 
+  writer << [product.id, product.title, product.date, product.location] 
+  end
+end
+  end
+
+
+  def popularTask()
+      table = Task.order("likes DESC").limit(2)
+      reportes(table)
+  end
+
+  def finishTask()
+      table = Task.select('title','responsible', 'status').where(status: ["Pendiente","Terminado"]); 
+      reportes(table)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,16 @@ Rails.application.routes.draw do
    
  
 
-   get "popular", to: "tasks#popular"
+     get "popular", to: "tasks#popular"
   
      get "tasks", to: "tasks#pendingFinish"
 
-     resources :tasks, only: [:index]
+     #resources :tasks, only: [:index]
+     #root 'api#index'
 
-root 'api#index'
+     get "report", to: "api#finishTask"
+     get "popularTask", to: "api#popularTask"
+     get "totalTask", to: "api#totalTask"
+     
 end
 
