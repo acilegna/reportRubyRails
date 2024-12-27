880a671c4fd920ffdcac04bd0c2a867fa3fc4432
acilegna <acilegna.airam88@gmail.com>
2024-12-26 22:33:08 -0600

    reporte CSV

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -2,41 +2,41 @@
 class ApiController < ApplicationController
 
 
-  def reportes(table)
-    require 'csv'
-    $user=ENV['USERNAME']
-    file = "C:/Users/#{$user}/Downloads/data.csv"        
-    CSV.open( file, 'w' ) do |writer|
-      writer << table.first.attributes.map { |a,v| a }
-      table.each do |s|
-        writer << s.attributes.map { |a,v| v }
-      end
-    end
-  end  
-  
-  def totalTask
-
+  def reportes(table,name)
     require 'csv' 
     $user=ENV['USERNAME']
-    file = "C:/Users/#{$user}/Downloads/data.csv"    
-    products = Task.order("likes DESC").limit(2)
-    headers = ["Product ID", "Name", "Price", "Description"]
-CSV.open(file, 'w', write_headers: true, headers: headers) do |writer|
-  products.each do |product| 
-  writer << [product.id, product.title, product.date, product.location] 
-  end
-end
-  end
+    file = "C:/Users/#{$user}/Downloads/reporte-#{name}.csv"    
+    headers = ["Id Tarea", "Title", "Description", "Location", "Status"]
+    CSV.open(file, 'w', write_headers: true, headers: headers) do |writer|
+      table.each do |task| 
+        writer << [task.id, task.title, task.description, task.location,task.status]       
+        
+      end    
+       writer <<  [ "\n" ]  
+       writer << ["totalTareas",   table.count]
+    end
+  end  
+ 
+
+ 
+  
+
+  #def totalTask
+   # @tasks=  Task.count
+    # render json: ["Tareas creadas", @tasks]    
+  #end
 
 
   def popularTask()
       table = Task.order("likes DESC").limit(2)
-      reportes(table)
+      name = 'popular';
+      reportes(table,name)
   end
 
   def finishTask()
-      table = Task.select('title','responsible', 'status').where(status: ["Pendiente","Terminado"]); 
-      reportes(table)
+      table = Task.where(status: ["Pendiente","Terminado"]); 
+      name = 'finish';
+      reportes(table, name)
   end
 
 end
