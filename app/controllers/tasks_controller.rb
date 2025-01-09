e36e85cb0ff7a80b44ec4dc2392ce6e58d0dcc5d
acilegna <acilegna.airam88@gmail.com>
2025-01-08 22:21:33 -0600

    pendiente2

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class TasksController < ApplicationController
   def shows1
     @task=  Task.order("likes DESC").limit(2)
@@ -7,51 +6,32 @@ class TasksController < ApplicationController
 
   def shows2
      @task= Task.count
-    
-   #render json: @task
+
+    # render json: @task
   end
 
    def allTasks
-      @task= Task.where(status: ["Pendiente","Terminado"])
+      @task= Task.where(status: [ "Pendiente", "Terminado" ])
 
-      
+
   respond_to do |format|
     format.xlsx {
       response.headers[
-        'Content-Disposition'
+        "Content-Disposition"
       ] = "attachment; filename='all.xlsx'"
     }
     format.html { render :allTasks }
   end
-=======
-class TasksController < ApplicationController  
-
-  
-def popular
-   @tasks=  Task.order("likes DESC").limit(2)
-    @total= Task.count
-
-    respond_to do |format|
-      format.html
-      format.xlsx {
-        response.headers['Content-Disposition'] = 'attachment; filename="popular.xlsx"'
-      }
-    end
->>>>>>> tmp
   end
 
     def pendingFinish
- 
-   @tasks= Task.where(status: ["Pendiente","Terminado"])
+   @tasks= Task.where(status: [ "Pendiente", "Terminado" ])
 
     respond_to do |format|
       format.html
       format.xlsx {
-        response.headers['Content-Disposition'] = 'attachment; filename="tasks.xlsx"'
+        response.headers["Content-Disposition"] = 'attachment; filename="tasks.xlsx"'
       }
     end
   end
-
-
-  
 end
