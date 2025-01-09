448340fd74649b2c90c9448ad5008764bdd6d81a
acilegna <acilegna.airam88@gmail.com>
2025-01-08 22:18:09 -0600

    pendiente

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,44 +1,40 @@
- 
+
 class ApiController < ApplicationController
-
-
   def reportes(table, name, total)
-    require 'csv' 
-    $user=ENV['USERNAME']
-    file = "C:/Users/#{$user}/Downloads/reporte-#{name}.csv"    
-    headers = ["Id Tarea", "Title", "Description", "Location", "Status"]
-    CSV.open(file, 'w', write_headers: true, headers: headers) do |writer|
-      table.each do |task| 
-        writer << [task.id, task.title, task.description, task.location,task.status]       
-        
-      end    
-       writer <<  [ "\n" ]  
-       writer << ["Total_tareas_creadas",   total]
+    require "csv"
+    $user=ENV["USERNAME"]
+    file = "C:/Users/#{$user}/Downloads/reporte-#{name}.csv"
+    headers = [ "Id Tarea", "Title", "Description", "Location", "Status" ]
+    CSV.open(file, "w", write_headers: true, headers: headers) do |writer|
+      table.each do |task|
+        writer << [ task.id, task.title, task.description, task.location, task.status ]
+      end
+       writer <<  [ "\n" ]
+       writer << [ "Total_tareas_creadas",   total ]
     end
-  end  
- 
+  end
 
- 
-  
+
+
+
 
 def totalTask
     @tasks=  Task.count
-     #render json: ["Tareas creadas", @tasks]    
+  # render json: ["Tareas creadas", @tasks]
 end
 
 
-  def popularTask()
+  def popularTask
       table = Task.order("likes DESC").limit(2)
-      name = 'popular';
+      name = "popular"
       total = totalTask
       reportes(table, name, total)
   end
 
-  def finishTask()
-      table = Task.where(status: ["Pendiente","Terminado"]); 
-      name = 'finish';
+  def finishTask
+      table = Task.where(status: [ "Pendiente", "Terminado" ])
+      name = "finish"
       total = totalTask
       reportes(table, name, total)
   end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,12 @@
 Rails.application.routes.draw do
-   
- 
-
      get "popular", to: "tasks#popular"
-  
+
      get "tasks", to: "tasks#pendingFinish"
 
-     #resources :tasks, only: [:index]
-     #root 'api#index'
+     # resources :tasks, only: [:index]
+     # root 'api#index'
 
      get "report", to: "api#finishTask"
      get "popularTask", to: "api#popularTask"
      get "totalTask", to: "api#totalTask"
-     
 end
-
